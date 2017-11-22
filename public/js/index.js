d69a340d27f9f4bb735f70806b86c8527c7352e6
hernandez.ramiro <hernandezramiro0804@gmail.com>
2017-11-22 17:53:39 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Jessica',
-        text: 'Yup. That works for me.'
-    });
 });
 
 socket.on('disconnect', function () {
