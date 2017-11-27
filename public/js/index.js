8b01e62fc513424f79da9d37a18d4504f31e9220
hernandez.ramiro <hernandezramiro0804@gmail.com>
2017-11-27 17:58:56 -0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,31 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    console.log('clientHeight', clientHeight);
+    console.log('scrollTop', scrollTop);
+    console.log('newMessageHeight', newMessageHeight);
+    console.log('lastMessageHeight', lastMessageHeight);
+
+    var aux = clientHeight + scrollTop + newMessageHeight + lastMessageHeight;
+
+    console.log('Total ' + aux + ' vs ScrollHeight ' + scrollHeight);
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -18,6 +44,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -30,6 +57,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
